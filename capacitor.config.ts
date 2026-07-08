9fff4ece25bd94e6753074d0e8fb9c5aec1f5333
Fabian <fabiiandres3@gmail.com>
2026-07-07 21:50:42 -0500

    Chayan

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.heyely.app',
+  appName: 'Hey Ely',
+  webDir: '.output/public',
+};
+
+export default config;
